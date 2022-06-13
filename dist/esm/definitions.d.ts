864d1e8f7a4e52250af0ca63940862980d61557d
Micah Lindley <micahincorporated@gmail.com>
2022-06-13 18:23:08 -0500

    Change peer deps

--- dist/esm/definitions.d.ts
+++ dist/esm/definitions.d.ts
@@ -1,9 +1,4 @@
 import { PluginListenerHandle } from "@capacitor/core";
-declare module '@capacitor/core' {
-    interface PluginRegistry {
-        DarkMode: DarkModePlugin;
-    }
-}
 export interface DarkModePlugin {
     isDarkModeOn(): Promise<any>;
     addListener(eventName: 'darkModeStateChanged', listenerFunc: (state: any) => void): PluginListenerHandle;
--- src/definitions.ts
+++ src/definitions.ts
@@ -1,11 +1,5 @@
 import { PluginListenerHandle } from "@capacitor/core";
 
-declare module '@capacitor/core' {
-  interface PluginRegistry {
-    DarkMode: DarkModePlugin;
-  }
-}
-
 export interface DarkModePlugin {
   isDarkModeOn(): Promise<any>;
   addListener(
